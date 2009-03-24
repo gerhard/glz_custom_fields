cd78ec80ed990e0d2c27e9959dd331ce857ef04e
Gerhard Lazu <mail@gerhardlazu.com>
2009-03-24 19:29:37 +0000

    Explained ranges a bit better
    Running our glz_custom_fields scripts only on the pages that we need them
    
    Sorted the search form

--- glz_custom_fields_raw.php
+++ glz_custom_fields_raw.php
@@ -4,7 +4,7 @@ glz_custom_fields plugin, FROM UK WITH LOVE
 
 @author Gerhard Lazu
 @version 1.2
-@copyright Gerhard Lazu, 21 January, 2009
+@copyright Gerhard Lazu, 24th March, 2009
 @package TXP 4.0.8 (r3078)
 @contributors:  Sam Weiss, redbot, Manfre, Vladimir, Julian Reisenberger, Steve Dickinson
 @special: Randy Levine, Husain Hakim
@@ -13,30 +13,34 @@ glz_custom_fields plugin, FROM UK WITH LOVE
 before_glz_custom_fields();
 
 if (txpinterface == "admin") {
-  // checks if all tables exist and everything is setup properly
-  glz_custom_fields_install();
   
-  // we need some stylesheets & JS
-  ob_start("glz_custom_fields_css_js");
-  
-  // these globals have been already set in before_glz_custom_fields() which is called everytime
-  global $all_custom_sets;
+  // we'll be doing this only on the pages that we care about, not everywhere
+  if ( in_array(gps('event'), array("article", "prefs", "glz_custom_fields")) ) {
+    // checks if all tables exist and everything is setup properly
+    glz_custom_fields_install();
+
+    // we need some stylesheets & JS
+    ob_start("glz_custom_fields_css_js");
+
+    // these globals have been already set in before_glz_custom_fields() which is called everytime
+    global $all_custom_sets;
+
+    // we need to make sure that all custom field values will be converted to strings first - think checkboxes & multi-selects
+    if ( (gps("step") == "edit") || (gps("step") == "create") ) {
+      add_privs('glz_custom_fields_before_save', "1");
+      register_callback('glz_custom_fields_before_save', "article", '', 1);
+    }
+    // if we edit or create a new article, save our extra custom fields (> 10)
+    if ( (gps("step") == "edit") || (gps("step") == "create") ) {
+      add_privs('glz_custom_fields_save', "1");
+      register_callback('glz_custom_fields_save', "article");
+    }
+  }
   
   // Custom Fields tab under Extensions
   add_privs('glz_custom_fields', "1");
   register_tab("extensions", 'glz_custom_fields', "Custom Fields");
   register_callback('glz_custom_fields', "glz_custom_fields");
-  
-  // we need to make sure that all custom field values will be converted to strings first - think checkboxes & multi-selects
-  if ( (gps("step") == "edit") || (gps("step") == "create") ) {
-    add_privs('glz_custom_fields_before_save', "1");
-    register_callback('glz_custom_fields_before_save', "article", '', 1);
-  }
-  // if we edit or create a new article, save our extra custom fields (> 10)
-  if ( (gps("step") == "edit") || (gps("step") == "create") ) {
-    add_privs('glz_custom_fields_save', "1");
-    register_callback('glz_custom_fields_save', "article");
-  }
   
   // YES, finally the default custom fields are replaced by the new, pimped ones : )
   add_privs('glz_custom_fields_replace', "1");
@@ -754,7 +758,6 @@ function glz_array_stripslashes(&$value) {
 // returns all sections/categories that are searchable
 function glz_all_searchable_sections_categories($type) {
   $type = (in_array($type, array('category', 'section')) ? $type : 'section');
-  
   $condition = "";
   
   if ( $type == "section" )
@@ -766,10 +769,10 @@ function glz_all_searchable_sections_categories($type) {
   
   $out = array();
   foreach ($result as $value) {
-    $out[] = $value['name'];
+    $out[$value['name']] = $value['title'];
   }
   
-  return join(",", $out);
+  return $out;
 }
 
 
@@ -1232,7 +1235,9 @@ function glz_mark_migration() {
  Call it like this: <txp:glz_custom_fields_search_form results_page="search" searchby="Area:input,City:select,Price:radio,section" section="default,articles" category="textpattern" />
  */
 function glz_custom_fields_search_form($atts) {
-  global $all_custom_sets, $s;
+  global $all_custom_sets, $s, $glz_search_options;
+  
+  $glz_search_options = array();
   
   // DEBUG
   // dmp($all_custom_sets);
@@ -1240,22 +1245,30 @@ function glz_custom_fields_search_form($atts) {
   // we have $_POST, let's see if it comes from glz_custom_fields_search
   // DEBUG
   // dmp($_POST);
-  $incoming = array();
   if ( $_POST AND in_array("glz_custom_fields_search", $_POST) ) {
     // stripPost() doesn't know how to handle arrays
     foreach ($_POST as $key => $value)
-      $incoming[$key] = (is_array($value)) ? stripslashes(join(",", $value)) : stripslashes($value);
+      $glz_search_options[$key] = (is_array($value)) ? stripslashes(join(",", $value)) : stripslashes($value);
   }
-  // DEBUG
-  // dmp($incoming);
   
   extract(lAtts(array(
     'results_page'  => "search",
     'searchby'      => "",
-    'section'       => "all",
-    'category'      => "all",
+    'section'       => "",
+    'category'      => "",
     'labels'        => 1
-  ),$atts));
+  ), $atts));
+  
+  if ( !isset($glz_search_options['section']) ) {
+    if ( $section != "" )
+      $glz_search_options['section'] = $section;
+  }
+  
+  if ( !isset($glz_search_options['category']) && $category != "" && $category != "all" )
+    $glz_search_options['category'] = $category;
+  
+  // DEBUG
+  // dmp($glz_search_options);
   
   if ( !empty($searchby) ) {
     // initialize our custom search array
@@ -1264,9 +1277,7 @@ function glz_custom_fields_search_form($atts) {
     // see which custom sets are searchby values associated to - if any
     if ( strstr($searchby, ",") ) {
       // go through values 1 by 1 and add them to the above array
-      foreach ( explode(",", $searchby) as $query_custom ) {
-        // strip whitespace from the beginning and end
-        $query_custom = trim($query_custom);
+      foreach ( do_list($searchby) as $key => $query_custom ) {
         // now we have types for our search fields
         // they are separated by :
         if ( strstr($query_custom, ":") )
@@ -1275,29 +1286,23 @@ function glz_custom_fields_search_form($atts) {
           // if we don't unset this, it will use the one from the previous searchby value
           unset($query_custom_type);
         
-        // get the values we have in our Prefs for this custom set - we might not get anything back
-        $custom = array_search($query_custom, $GLOBALS['prefs']);
-        
-        if ( $custom ) {
-          // let's make sure we have a custom set type before building our custom sets search array
-          if (!isset($query_custom_type))
-            list($query_custom, $query_custom_type) = array_values($all_custom_sets[$custom]);
+        // if this is a section or category, we just need to set the type
+        if ( in_array($query_custom, array("section", "category")) ) {
+          $query_custom_type = "checkbox";
+          $custom = $key;
         }
-        // otherwise we're dealing with a section, category or search input
+        // otherwise we need to check for the type of this custom field
         else {
-          if ( !isset($query_custom_type) ) {
-            switch($query_custom) {
-              case "section":
-                $query_custom_type = "checkbox";
-                break;
-              case "category":
-                $query_custom_type = "checkbox";
-                break;
-              default:
-                $query_custom_type = "text_input";
-                break;
+          // get the values we have in our Prefs for this custom set - we might not get anything back
+          $custom = array_search($query_custom, $GLOBALS['prefs']);
+          
+           if ( $custom ) {
+              if ( !isset($query_custom_type) )
+                list($query_custom, $query_custom_type) = array_values($all_custom_sets[$custom]);
             }
-          }
+            // if we don't get a custom back, lets default to search input
+            else
+              $query_custom_type = "text_input";
         }
         // add this custom set to our search array
         $arr_query_custom[$custom] = array(
@@ -1308,7 +1313,10 @@ function glz_custom_fields_search_form($atts) {
     else
       // we are searching for a single custom set, add it to our custom sets search array
       $arr_query_custom[$searchby] = array_search($query_custom_set, $GLOBALS['prefs']);
-      
+    
+    // DEBUG
+    // dmp($arr_query_custom);
+    
     // start our form
     $out[] = '<form method="post" action="'.hu.$results_page.'">'.n
       .'<fieldset>'.n;
@@ -1328,35 +1336,42 @@ function glz_custom_fields_search_form($atts) {
         $custom = $custom_id;
       }
       
-      // get all existing values for live articles
-      $arr_custom_values = glz_custom_fields_MySQL('all_values', $custom, '', array('custom_set_name' => $custom_search['name'], 'status' => 4));
+      // values for sections and categories have already been defined
+      if ( in_array($custom, array("section", "category")) ) {
+        if ( $$custom == "all" )
+            $arr_custom_values = glz_all_searchable_sections_categories($custom);
+        else
+          continue;
+      }
+      // get all existing custom values for live articles
+      else
+        $arr_custom_values = glz_custom_fields_MySQL('all_values', $custom, '', array('custom_set_name' => $custom_search['name'], 'status' => 4));
+      
       // DEBUG
       // dmp($arr_custom_values);
       
       if ( is_array($arr_custom_values) ) {
-        $checked_value = (isset($incoming[$custom])) ? $incoming[$custom] : '';
+        $checked_value = (isset($glz_search_options[$custom])) ? $glz_search_options[$custom] : '';
+        
+        if ( in_array($custom, array("section", "category")) )
+          $checked_value = str_replace(",", "|", $checked_value);
+        
         // join the values if they are multiple ones e.g. coming in an array
         if ( is_array($checked_value) )
           $checked_value = join("|", $checked_value);
         // the way our custom field value is going to look like
         list($custom_value, $custom_class) = glz_format_custom_set_by_type($custom, $custom_id, $custom_search['type'], $arr_custom_values, $checked_value);
-        
+      
         // DEBUG
         // dmp($custom_value);
         // dmp($custom_search);
-        
+      
         $out[] = ($labels) ?
           graf("<label for=\"$custom_id\">{$custom_search['name']}</label><br />$custom_value", " class=\"$custom_class\"") :
           graf("$custom_value", " class=\"$custom_class\"");
       }
       else
         return '<p>'.glz_custom_fields_gTxt('no_articles_found').'</p></form>';
-    }
-    
-    // there will be default sections & categories
-    foreach (array("section", "category") as $value) {
-      $values = (($$value == "all") ? glz_all_searchable_sections_categories($value, $$value) : $$value);
-      $out[] = hInput("{$value}['default']", $values);
     }
     
     // end our form
@@ -1400,7 +1415,7 @@ function glz_custom_fields_install() {
   // if jQuery is not present, trigger error
   // improvement courtesy of Sam Weiss
   if ( !file_exists($GLOBALS['txpcfg']['txpath'].'/jquery.js') ) {
-    trigger_error(glz_custom_fields_gTxt('jquery_missing_check'));
+    trigger_error(glz_custom_fields_gTxt('jquery_missing'));
     trigger_error(glz_custom_fields_gTxt('check_path'));
   }
   
