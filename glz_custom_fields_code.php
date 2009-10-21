49b45194ba15b550a195ae998d9e8cdc3e20cddf
Gerhard Lazu <mail@gerhardlazu.com>
2009-10-21 20:15:12 +0100

    Custom set names are now stripped of \W characters

--- glz_custom_fields_code.php
+++ glz_custom_fields_code.php
@@ -93,6 +93,8 @@ function glz_custom_fields() {
       if ( !$custom_set_name )
         $glz_notice[] = glz_custom_fields_gTxt("no_name");
       else {
+        $custom_set_name = preg_replace('/[^A-Za-z0-9\s]/', '', $custom_set_name);
+
         $name_exists = glz_check_custom_set_name($all_custom_sets, $custom_set_name);
 
         // if name doesn't exist
@@ -129,6 +131,7 @@ function glz_custom_fields() {
     // we are editing an existing custom field
     if ( gps('save') ) {
       if ( !empty($custom_set_name) ) {
+        $custom_set_name = preg_replace('/[^A-Za-z0-9\s]/', '', $custom_set_name);
         $name_exists = glz_check_custom_set_name($all_custom_sets, $custom_set_name, $custom_set);
         // if name doesn't exist
         if ( $name_exists == FALSE ) {
