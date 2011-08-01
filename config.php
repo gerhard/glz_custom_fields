e083cca6ce3544b35146eaacad337ced7e46c905
Gerhard Lazu <gerhard@lazu.co.uk>
2011-08-01 01:21:24 +0100

    setup (TXP) & package (plugin) scripts for convenience

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+$txpcfg['db'] = 'textpattern';
+$txpcfg['user'] = 'root';
+$txpcfg['pass'] = '';
+$txpcfg['host'] = 'localhost';
+$txpcfg['table_prefix'] = '';
+$txpcfg['txpath'] = '/Users/gerhard/code/www/textpattern/textpattern';
+$txpcfg['dbcharset'] = 'utf8';
+?>
